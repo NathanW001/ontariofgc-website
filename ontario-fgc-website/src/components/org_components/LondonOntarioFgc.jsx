31db37f19a48750d2b43c6e430f9d1bbee21b88c
Nathan <nathan.wootton1@gmail.com>
2025-01-11 03:16:32 -0500

    fixed info in london fgc

--- ontario-fgc-website/src/components/org_components/LondonOntarioFgc.jsx
+++ ontario-fgc-website/src/components/org_components/LondonOntarioFgc.jsx
@@ -4,7 +4,7 @@ import london_ontario_fgc_logo from '../../assets/orgs/london_ontario_fgc_logo.p
 function LondonOntarioFgc() {
   return (
     <>
-      <Default title="London Ontario FGC" logo={london_ontario_fgc_logo} body="The home of London Ontario Fighting Game Community! They host two weekly meetups, one on Wednesdays at the Poacher's Arms in Midtown London and one on Fridays on the Western University campus. They run a variety of different games, including Tekken 8, Street Fighter 6 and Guilty Gear Strive." discord_url={"https://discord.gg/YaPRRte"} youtube_url={"https://www.youtube.com/@londonontariofgc6552"} twitter_url={"https://x.com/LondonOntFGC"} twitch_url={"https://www.twitch.tv/londonontariofgc"}/>
+      <Default title="London Ontario FGC" logo={london_ontario_fgc_logo} body="The home of London Ontario Fighting Game Community! They host two weekly meetups, one on Wednesdays at the Poacher's Arms in Midtown London and one on Fridays on the Western University campus. They run a variety of different games, including popular titles such as Tekken 8, Street Fighter 6, Guilty Gear Strive, Guilty Gear Xrd, King of Fighters, Blazblue, Marvel vs Capcom, and more. They also have a strong community around legacy titles such as CVS2 and ST." discord_url={"https://discord.gg/YaPRRte"} youtube_url={"https://www.youtube.com/@londonontariofgc6552"} twitter_url={"https://x.com/LondonOntFGC"} twitch_url={"https://www.twitch.tv/londonontariofgc"}/>
     </>
   )
 }
